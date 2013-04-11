4adc6a951f69662ed4aad05513f79f3a5cbb7cf8
tekkno <tekkno@terminator.lan>
2013-04-11 20:49:54 +0200

    first source files

---
+++ Makrfile.rb
@@ -0,0 +1,123 @@
+
+Makr.loadExtension("ToolChainLinuxGcc")
+
+
+# "parse" arguments
+$arguments = Makr.getArgs()
+$localDir = File.dirname($arguments.scriptFile)
+$buildDir = $arguments.arguments[0]
+$target = $arguments.arguments[1]
+
+# set log level to info
+Makr.log.level = Logger::INFO
+
+
+$eigenDir = "/mnt/programmieren/math_libs/eigen"
+$boostDir = "/mnt/programmieren/current_boost"
+
+
+def configureBase()
+  compilerConfig = $build.makeNewConfig("CompileTaskCPP")
+  compilerConfig.clear()
+  compilerConfig["compiler.cFlags"] = " -Wall -Werror -fexceptions -std=c++11"
+  compilerConfig["compiler.includePaths"] = " -I#{$localDir}/src -I#{$eigenDir} -I#{$boostDir}"
+end
+
+
+def configureMaxDebug()
+  compilerConfig = $build.getConfig("CompileTaskCPP")
+  compilerConfig["compiler.cFlags"] += " -ggdb -fstack-check  -fno-eliminate-unused-debug-types"
+  compilerConfig["compiler.defines"] = " -DDEBUG -D_GLIBCXX_DEBUG"
+end
+
+
+def configureStdLibDebug()
+  compilerConfig = $build.getConfig("CompileTaskCPP")
+  compilerConfig["compiler.cFlags"] += " -ggdb -fno-eliminate-unused-debug-types"
+  compilerConfig["compiler.defines"] = " -DDEBUG -D_GLIBCXX_DEBUG"
+end
+
+
+def configureDebug()
+  compilerConfig = $build.getConfig("CompileTaskCPP")
+  compilerConfig["compiler.cFlags"] += " -ggdb -fno-eliminate-unused-debug-types"
+  compilerConfig["compiler.defines"] = " -DDEBUG"
+end
+
+
+def configureReleaseStdLibDebug()
+  compilerConfig = $build.getConfig("CompileTaskCPP")
+  compilerConfig["compiler.cFlags"] += " -O2 -ggdb -fno-eliminate-unused-debug-types"
+  compilerConfig["compiler.defines"] = " -D_GLIBCXX_DEBUG"
+end
+
+
+def configureReleaseDebug()
+  compilerConfig = $build.getConfig("CompileTaskCPP")
+  compilerConfig["compiler.cFlags"] += " -O2 -ggdb -fno-eliminate-unused-debug-types"
+  compilerConfig["compiler.defines"] = " -DDEBUG"
+end
+
+
+def configureRelease()
+  compilerConfig = $build.getConfig("CompileTaskCPP")
+  compilerConfig["compiler.cFlags"] += " -O3"
+end
+
+
+def configureProfiling()
+  compilerConfig = $build.getConfig("CompileTaskCPP")
+  compilerConfig["compiler.cFlags"] += " -O3 -pg"
+end
+
+
+def configure()
+
+  configureBase()
+
+  case $buildDir
+    when "MaxDebug"
+      configureMaxDebug()
+    when "StdLibDebug"
+      configureStdLibDebug()
+    when "Debug"
+      configureDebug()
+    when "ReleaseStdLibDebug"
+      configureReleaseStdLibDebug()
+    when "ReleaseDebug"
+      configureReleaseDebug()
+    when "Release"
+      configureRelease()
+    when "Profiling"
+      configureProfiling()
+    else
+      puts "unknown target, aborting"
+      Makr.abortBuild()
+  end
+end
+
+
+def buildIt()
+  # first get build
+  $build = Makr.loadBuild(Makr.cleanPathName($buildDir))
+  # then use build block concept to ensure the build is saved
+  $build.saveAfterBlock do
+    configure()
+
+    allCPPFiles = Makr::FileCollector.collect($localDir + "/src/", "*.{cpp,cxx}", true)
+    tasks = Makr.applyGenerators(allCPPFiles, [Makr::CompileTaskGenerator.new($build, $build.getConfig("CompileTaskCPP"))])
+
+    #myLibTask = Makr.makeStaticLib($buildDir + "/libuenf-graph.a", $build, tasks, $build.getConfig("CompileTaskCPP"))
+    myProg = Makr.makeProgram($buildDir + "/uenftest", $build, tasks, $build.getConfig("CompileTaskCPP"))
+
+    $build.build()
+  end  
+end
+
+
+# main logic ###########################################################################################
+if($target == "clean")
+  system("rm -f " + $buildDir + "/*")
+else
+  buildIt()
+end
---
+++ src/uenf/glInterface/Node.h
@@ -0,0 +1,71 @@
+#ifndef UENF_GLINTERFACE_NODE_H
+#define UENF_GLINTERFACE_NODE_H
+
+// Copyright 2013 (c) by Thorsten Gecks (see also the file LICENSE in the base directory (its MIT))
+
+
+
+
+#include <uenf/glInterface/GLPassInformation.h>
+
+
+#ifdef UENF_GLINTERFACE_TRYTYPEFIELD
+#include <uenf/glInterface/NodeType.h>
+#endif
+
+
+#include <memory>
+
+
+
+
+namespace uenf
+{
+
+namespace glInterface
+{
+
+
+//! As we have no virtuals (in shared memory scenarios), we will use a type field.
+class Node
+{
+public:    
+  std::weak_ptr<Node> const & getParent() const { return parent; }
+  void getParent(std::weak_ptr<Node> const & parentArg) { parent = parentArg; }
+  
+  #ifdef UENF_GLINTERFACE_TRYTYPEFIELD
+  Node(NodeType const & typeArg = NodeType::None):type(typeArg){}
+
+  NodeType const & getNodeType() const { return nodeType; }
+  #endif
+
+  // All derived may just overwrite the following functions. This is essentially defining an interface,
+  // but only by convention, not using virtual functions, because we want to simplify inlining and be
+  // able to use the class in shared memory scenarios (thus no virtuals)
+  #ifdef UENF_GLINTERFACE_TRYTYPEFIELD
+  void update(){}
+  #else
+  virtual void update() = 0;
+  #endif
+  
+  #ifdef UENF_GLINTERFACE_TRYTYPEFIELD
+  void execute(GLPassInformation pass){}
+  #else
+  virtual void execute(GLPassInformation const & pass) = 0;
+  #endif
+  
+private:
+  std::weak_ptr<Node> parent;
+  
+  #ifdef UENF_GLINTERFACE_TRYTYPEFIELD
+  const NodeType type;
+  #endif
+};
+
+  
+} // namespace glInterface
+
+} // end of namespace uenf
+
+#endif // UENF_GLINTERFACE_NODE_H
+
