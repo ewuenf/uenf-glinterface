6ba426d61671e20d200af90db694086cbe592368
tekkno <tekkno@terminator.lan>
2013-04-11 20:52:58 +0200

    type field as an alternative to virtual functions

---
+++ src/uenf/glInterface/TypeFieldEnums.h
@@ -0,0 +1,30 @@
+#ifndef UENF_GLINTERFACE_TRYTYPEFIELD_H
+#define UENF_GLINTERFACE_TRYTYPEFIELD_H
+
+#include <cstdint>
+#include <limits>
+
+
+namespace uenf
+{
+
+namespace glInterface
+{
+
+typedef uint32_t NodeTypeBase;
+
+enum class NodeType : NodeTypeBase
+{
+  None = 0,
+  Test = 1,
+  User = std::numeric_limits<NodeTypeBase>::max() / 2,
+  Max = User * 2      
+};
+
+  
+} // namespace glInterface
+
+} // end of namespace uenf
+
+#endif // UENF_NODE_H
+
